c8ab348a65fc3483b676e3216b34087364e0fcbe
Dhony Abu Muhammad <76083017+4bumuhammad@users.noreply.github.com>
2024-03-14 17:42:24 +0700

    updates

--- small-applications/002-python-imagekit-checkurl.py
+++ small-applications/002-python-imagekit-checkurl.py
@@ -8,8 +8,12 @@ class ImageChecker:
     def is_url_image(self, image_url):
         img_formats = ("image/png", "image/jpeg", "image/jpg")
         try:
-            r = requests.head(image_url)
-            if r.headers["content-type"] in img_formats:
+
+            response = requests.get(image_url, stream=True)
+            content_type = response.headers.get('content-type', '')
+            content_length = response.headers.get('content-length', '')
+
+            if response.status_code == 200 and content_type in img_formats and content_length:
                 return True
             return False
         except Exception as e:
@@ -24,8 +28,10 @@ if __name__ == "__main__":
     image_url = sys.argv[1]
     checker = ImageChecker()
 
-    # Check if the provided URL points to an imageß
     if checker.is_url_image(image_url):
         print("The provided URL points to an image.")
     else:
         print("The provided URL does not point to an image or cannot be reached.")
+
+
+# #### all of the following URL lists have basically been removed from the Image Kit media-library ####
--- small-applications/004-python-imagekit-purgecache.py
+++ small-applications/004-python-imagekit-purgecache.py
@@ -1,25 +1,59 @@
+import sys
+import time
 from imagekitio import ImageKit
 
-# Informasi kredensial
-private_key = 'private_tMllcpEGq4gf7TzbqEWpqQzupac='
-public_key = 'public_Gw9y5gNm5ZfCzIfa49kR3QUJ1dA='
-url_endpoint = 'https://ik.imagekit.io/2fbeg9wdr'
-url_image = 'https://ik.imagekit.io/2fbeg9wdr/mobil-hijau_2MGgmanuQ.jpg'
+class ImageCachePurger:
+    def __init__(self, private_key, public_key, url_endpoint):
+        self.imagekit = ImageKit(
+            private_key=private_key,
+            public_key=public_key,
+            url_endpoint=url_endpoint
+        )
 
-# Inisialisasi ImageKit
-imagekit = ImageKit(
-    private_key=private_key,
-    public_key=public_key,
-    url_endpoint=url_endpoint
-)
+    def purge_cache(self, url):
+        try:
+            print("- Purging cache for the image...")
+            purge_response = self.imagekit.purge_cache(url)
+            request_id = purge_response.request_id
+            print("Request ID:", request_id)
 
-# Fungsi untuk membersihkan cache
-def purge_cache(url):
-    response = imagekit.purge_cache(url)
-    if response.status == 'success':
-        print("Cache berhasil dihapus untuk:", url)
-    else:
-        print("Gagal menghapus cache.")
+            polling_count = 0
 
-# Memanggil fungsi untuk membersihkan cache
-purge_cache(url_image)
+            while True:
+
+                purge_cache_status = self.imagekit.get_purge_cache_status(purge_cache_id=request_id)
+
+                if polling_count <= 2 and purge_cache_status.status == 'Pending':
+                    print("Cache status: Pending")
+                
+                if polling_count > 2 and purge_cache_status.status == 'Pending':
+                    print("Cache status: In progress")
+                
+                if purge_cache_status.status == 'Completed':
+                    break
+                
+                polling_count += 1
+                time.sleep(1)  
+            
+            return purge_cache_status.status
+
+        except Exception as e:
+            print("- Failed to purge cache:", str(e))
+            return None
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 004-python-imagekit-purgecache.py <url_image>")
+        sys.exit(1)
+
+    private_key = 'private_tMllcpEGq4gf7TzbqEWpqQzupac='
+    public_key = 'public_Gw9y5gNm5ZfCzIfa49kR3QUJ1dA='
+    url_endpoint = 'https://ik.imagekit.io/2fbeg9wdr'
+
+    url_image = sys.argv[1]
+
+    cache_purger = ImageCachePurger(private_key, public_key, url_endpoint)
+
+    purge_result = cache_purger.purge_cache(url_image)
+    if purge_result:
+        print("Purge result:", purge_result)
